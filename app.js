e51bea105464d8c8c31761cca1fcc2384bf0c92d
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2022-05-09 21:45:52 +0300

    need to continue pacman and settings

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var pac_color;
 var start_time;
 var time_elapsed;
 var interval;
-var pac_img;
+var food_counter;
+var base_image = new Image();
 
 $(document).ready(function() {
 	context = canvas.getContext("2d");
@@ -37,7 +38,7 @@ function Start() {
 		['X','X','X','X','X','X','.','X','X','.','.','.','.','.','.','.','.','.','.','X','X','.','X','X','X','X','X','X'],
 		['X','X','X','X','X','X','.','X','X','.','X','X','X','.','.','X','X','X','.','X','X','.','X','X','X','X','X','X'],
 		['X','X','X','X','X','X','.','X','X','.','X','.','.','.','.','.','.','X','.','X','X','.','X','X','X','X','X','X'],
-		['.','.','.','.','.','.','.','.','.','.','X','.','.','.','1','.','.','X','.','.','.','.','.','.','.','.','.','.'],
+		['X','.','.','.','.','.','.','.','.','.','X','.','.','1','.','.','.','X','.','.','.','.','.','.','.','.','.','X'],
 		['X','X','X','X','X','X','.','X','X','.','X','.','.','.','.','.','.','X','.','X','X','.','X','X','X','X','X','X'],
 		['X','X','X','X','X','X','.','X','X','.','X','X','X','X','X','X','X','X','.','X','X','.','X','X','X','X','X','X'],
 		['X','X','X','X','X','X','.','X','X','.','.','.','.','.','.','.','.','.','.','X','X','.','X','X','X','X','X','X'],
@@ -66,7 +67,6 @@ function Start() {
 	shape.i = 14
 	shape.j = 14
 	pacman_remain--;
-	
 	keysDown = {};
 	addEventListener(
 		"keydown",
@@ -115,14 +115,16 @@ function Draw() {
 	lblScore.value = score;
 	lblTime.value = time_elapsed;
 	pac_img = new Image()
-	pac_img.src = 'media/logo.png'
+	pac_img.src = 'media/pacman_icon.png'
+	let is_painted=false
+	let last_hope=[0,0]
 
 	for (var i = 0; i < board.length; i++) {
 		for (var j = 0; j < board[i].length; j++) {
 			var center = new Object();
 			center.x = i * 30 + 5;
 			center.y = j * 30 + 5;
-			 if (board[i][j] == '.') {
+		 	if (board[i][j] == '.') {
 				context.beginPath();
 				context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
 				context.fillStyle = "white"; //color
@@ -130,41 +132,64 @@ function Draw() {
 			} else if (board[i][j] == 'X') {
 				context.beginPath();
 				context.rect(center.x - 20, center.y - 20, 40, 40);
-				context.fillStyle = "black"; //color
+				context.lineWidth = "50";
+				context.strokeStyle = "blue";
+				context.fillStyle = "blue"; //color
 				context.fill();
-			} else if(board[i][j] == '1'){
-				// context.drawImage(pac_img, center.x, center.y)
-				// context.beginPath();
-				// context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // half circle
-				// context.lineTo(center.x, center.y);
-				// context.fillStyle = pac_color; //color
-				// context.fill();
 			}
+			 else if(board[i][j] == '1' ||board[i][j] == '_' ){
+				 console.log(i)
+				console.log(j)
+				// x=Math.random()
+				// if (x>0.2) {
+					console.log("herreeee")
+					base_image.src = 'media/pacman_icon.png';
+					context.drawImage(base_image, shape.i*30-5 , shape.j*30-5 , 20, 20);
+
+				// }
+			 }
+
 		}
 	}
+
+
 }
 
+
+
+
 function UpdatePosition() {
-	board[shape.i][shape.j] = 0;
+	board[shape.i][shape.j] = '_';
+	console.log("herreeee")
+	base_image.src = 'media/pacman_icon.png';
+	context.drawImage(base_image, shape.i , shape.j , 40, 40);
 	var x = GetKeyPressed();
 	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
+		if (shape.j > 0 && board[shape.i][shape.j - 1] != 'X') {
 			shape.j--;
+			board[shape.i][shape.j] = '1';
+
 		}
 	}
 	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
+		if (shape.j < board[0].length && board[shape.i][shape.j + 1] != 'X') {
 			shape.j++;
+			board[shape.i][shape.j] = '1';
+
 		}
 	}
 	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
+		if (shape.i > 0 && board[shape.i - 1][shape.j] != 'X') {
 			shape.i--;
+			board[shape.i][shape.j] = '1';
+
 		}
 	}
 	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
+		if (shape.i < board.length && board[shape.i + 1][shape.j] != 'X') {
 			shape.i++;
+			board[shape.i][shape.j] = '1';
+
 		}
 	}
 	if (board[shape.i][shape.j] == '.') {
@@ -176,7 +201,7 @@ function UpdatePosition() {
 	if (score >= 20 && time_elapsed <= 10) {
 		pac_color = "green";
 	}
-	if (score == 50) {
+	if (score == food_counter) {
 		window.clearInterval(interval);
 		window.alert("Game completed");
 	} else {

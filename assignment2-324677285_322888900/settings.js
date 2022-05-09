37b724e567fd0caa9c6f3caa8554e909ca82f134
Nivfono2001 <81295283+Nivfono2001@users.noreply.github.com>
2022-05-09 21:55:43 +0300

    need to continue pacman and settings

---
+++ assignment2-324677285_322888900/settings.js
@@ -0,0 +1,21 @@
+var up_button;
+var down_button;
+var left_button;
+var right_button;
+var number_of_balls;
+var color_five_point_ball;
+var color_fifteen_point_ball;
+var color_twenty_five_point_ball;
+var time_counter;
+var number_of_monsters;
+
+
+//
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+// output.innerHTML = slider.value;
+//
+// slider.oninput = function() {
+//     output.innerHTML = this.value;
+// }
+
